4c2dc800175ca508cf63af394b6b3ac87dedf74d
Nicholas Sotiriou <nsotiriou88@users.noreply.github.com>
2020-08-05 17:32:43 +0100

    [add] weo and iv in Python

--- 1 - Data Preprocessing/README.md
+++ 1 - Data Preprocessing/README.md
@@ -3,3 +3,14 @@
 ## Intro
 
 Samples of code for Data Preprocessing.
+
+
+## Weight of Evidence and Information Value
+
+This is for feature selection process:
+
+- Less than 0.02	Not useful for prediction
+- 0.02 to 0.1	Weak predictive Power
+- 0.1 to 0.3	Medium predictive Power
+- 0.3 to 0.5	Strong predictive Power
+- \>0.5	Suspicious Predictive Power
---
+++ 1 - Data Preprocessing/woi_iv.py
@@ -0,0 +1,44 @@
+#Load Required Packages
+import pandas as pd
+import numpy as np
+
+#Load Data
+mydata = pd.read_csv("https://stats.idre.ucla.edu/stat/data/binary.csv")
+mydata.to_pickle('data.pkl')
+
+def iv_woe(data, target, bins=10, show_woe=False):
+    
+    #Empty Dataframe
+    newDF,woeDF = pd.DataFrame(), pd.DataFrame()
+    
+    #Extract Column Names
+    cols = data.columns
+    
+    #Run WOE and IV on all the independent variables
+    for ivars in cols[~cols.isin([target])]:
+        if (data[ivars].dtype.kind in 'bifc') and (len(np.unique(data[ivars]))>10):
+            binned_x = pd.qcut(data[ivars], bins,  duplicates='drop')
+            d0 = pd.DataFrame({'x': binned_x, 'y': data[target]})
+        else:
+            d0 = pd.DataFrame({'x': data[ivars], 'y': data[target]})
+        d = d0.groupby("x", as_index=False).agg({"y": ["count", "sum"]})
+        d.columns = ['Cutoff', 'N', 'Events']
+        d['% of Events'] = np.maximum(d['Events'], 0.5) / d['Events'].sum()
+        d['Non-Events'] = d['N'] - d['Events']
+        d['% of Non-Events'] = np.maximum(d['Non-Events'], 0.5) / d['Non-Events'].sum()
+        d['WoE'] = np.log(d['% of Events']/d['% of Non-Events'])
+        d['IV'] = d['WoE'] * (d['% of Events'] - d['% of Non-Events'])
+        d.insert(loc=0, column='Variable', value=ivars)
+        print("Information value of " + ivars + " is " + str(round(d['IV'].sum(),6)))
+        temp =pd.DataFrame({"Variable" : [ivars], "IV" : [d['IV'].sum()]}, columns = ["Variable", "IV"])
+        newDF=pd.concat([newDF,temp], axis=0)
+        woeDF=pd.concat([woeDF,d], axis=0)
+
+        #Show WOE Table
+        if show_woe == True:
+            print(d)
+    return newDF, woeDF
+
+iv, woe = iv_woe(data = mydata, target = 'admit', bins=10, show_woe = True)
+print(iv)
+print(woe)
